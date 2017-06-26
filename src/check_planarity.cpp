9cfbb1aae7861f328b58559f03bd82f0d16dfd4a
Albert Palomer <albert.palo@gmail.com>
2017-06-26 19:25:05 +0200

    Added the standard deviation and also the published cloud is coloured acording to the fitting error

--- src/check_planarity.cpp
+++ src/check_planarity.cpp
@@ -23,6 +23,51 @@ double point2planedistnace(pcl::PointXYZ pt, pcl::ModelCoefficients::Ptr coeffic
     return f1/f2;
 }
 
+class ColorMap{
+public:
+    ColorMap(double mn, double mx): mn(mn), mx(mx){}
+    void setMinMax(double min, double max){ mn = min; mx = max;}
+    void setMin(double min){mn = min;}
+    void setMax(double max){mx = max;}
+    void getColor(double c,uint8_t& R, uint8_t& G, uint8_t& B){
+        double normalized = (c - mn)/(mx-mn) * 2 - 1;
+        R = (int) (base(normalized - 0.5) * 255);
+        G = (int) (base(normalized) * 255);
+        B = (int) (base(normalized + 0.5) * 255);
+    }
+    void getColor(double c, double &rd, double &gd, double &bd){
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        getColor(c,r,g,b);
+        rd = (double)r/255;
+        gd = (double)g/255;
+        bd = (double)b/255;
+    }
+    uint32_t getColor(double c){
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        getColor(c,r,g,b);
+        return ((uint32_t)r<<16|(uint32_t)g<<8|(uint32_t)b);
+    }
+
+
+private:
+    double interpolate(double val, double y0, double x0, double y1, double x1){
+        return (val - x0)*(y1-y0)/(x1-x0) + y0;
+    }
+    double base(double val){
+        if (val <= -0.75) return 0;
+        else if (val <= -0.25) return interpolate(val,0,-0.75,1,-0.25);
+        else if (val <= 0.25) return 1;
+        else if (val <= 0.75) return interpolate(val,1.0,0.25,0.0,0.75);
+        else return 0;
+    }
+private:
+    double mn,mx;
+};
+
 class Color{
 private:
     uint8_t r;
@@ -128,29 +173,46 @@ public:
             // Iterate inliers
             double mean_error(0);
             double max_error(0);
-            uint32_t rgb = colors[n_planes].getColor();
+            double min_error(100000);
+            std::vector<double> err;
             for (int i=0;i<inliers->indices.size();i++){
 
                 // Get Point
                 pcl::PointXYZ pt = cloud->points[inliers->indices[i]];
 
                 // Compute distance
-                double d = point2planedistnace(pt,coefficients);
+                double d = point2planedistnace(pt,coefficients)*1000;// mm
+                err.push_back(d);
 
                 // Update statistics
                 mean_error += d;
                 if (d>max_error) max_error = d;
+                if (d<min_error) min_error = d;
+
+            }
+            mean_error/=inliers->indices.size();
+
+            // Compute Standard deviation
+            ColorMap cm(min_error,max_error);
+            double std(0);
+            for (int i=0;i<inliers->indices.size();i++){
+
+                std += pow(err[i] - mean_error,2);
+
+                // Get Point
+                pcl::PointXYZ pt = cloud->points[inliers->indices[i]];
 
                 // Copy point to noew cloud
                 pcl::PointXYZRGB pt_color;
                 pt_color.x = pt.x;
                 pt_color.y = pt.y;
                 pt_color.z = pt.z;
+                uint32_t rgb = cm.getColor(err[i]);
                 pt_color.rgb = *reinterpret_cast<float*>(&rgb);
                 cloud_pub->points.push_back(pt_color);
 
             }
-            mean_error/=inliers->indices.size();
+            std = sqrt(std/inliers->indices.size());
 
             // Extract inliers
             extract.setInputCloud(cloud);
@@ -162,7 +224,7 @@ public:
 
             // Display infor
             ROS_INFO("%s: fitted plane %i: %fx+%fy+%fz+%f=0 (inliers: %zu/%i)",_name.c_str(),n_planes,coefficients->values[0],coefficients->values[1],coefficients->values[2],coefficients->values[3],inliers->indices.size(),original_size);
-            ROS_INFO("%s: mean error: %f(mm), max error: %f(mm)",_name.c_str(),mean_error*1000,max_error*1000);
+            ROS_INFO("%s: mean error: %f(mm), standard deviation: %f (mm), max error: %f(mm)",_name.c_str(),mean_error,std,max_error);
             ROS_INFO("%s: poitns left in cloud %i",_name.c_str(),cloud->width*cloud->height);
 
             // Nest iteration
